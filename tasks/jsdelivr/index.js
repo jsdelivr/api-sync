37e3d3ae46ce0154cab6c9322ad0ce3ee2034e9d
Juho Vepsalainen <bebraw@gmail.com>
2014-05-06 13:59:14 +0300

    jsdelivr - Fetch data through GitHub instead of PHP

--- tasks/jsdelivr/index.js
+++ tasks/jsdelivr/index.js
@@ -1,31 +1,24 @@
 'use strict';
 
 var async = require('async');
-var request = require('request');
 var sugar = require('mongoose-sugar');
 
+var scrape = require('./scrape');
 var sortVersions = require('../../lib/sort_versions');
 var Library = require('../../schemas').jsdelivrLibrary;
 
 
 module.exports = function(cb) {
-    var url = 'http://www.jsdelivr.com/packagesmain.php';
-
     console.log('Starting to update jsdelivr data');
 
-    request.get({
-        url: url,
-        json: true
-    }, function(err, res, data) {
-        if(err || !data || !data.package) {
-            console.error('Failed to update jsdelivr data!', err, data);
+    scrape(function(err, files) {
+        if(err) {
+            console.error('Failed to update jsdelivr data!', err);
 
             return cb(err);
         }
 
-        console.log('Fetched jsdelivr data');
-
-        async.each(data.package, function(library, cb) {
+        async.each(files, function(library, cb) {
             sugar.getOrCreate(Library, {
                 name: library.name
             }, function(err, d) {
@@ -33,6 +26,7 @@ module.exports = function(cb) {
                     return cb(err);
                 }
 
+                library.zip = library.name + '.zip';
                 library.versions = sortVersions(library.versions);
 
                 sugar.update(Library, d._id, library, cb);
---
+++ tasks/jsdelivr/scrape.js
@@ -0,0 +1,172 @@
+'use strict';
+
+var url = require('url');
+
+var async = require('async');
+var extend = require('extend');
+var fp = require('annofp');
+var prop = fp.prop;
+var values = fp.values;
+
+var request = require('request');
+var ini = require('ini');
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+    version: '3.0.0',
+    protocol: 'https',
+    timeout: 5000
+});
+
+
+module.exports = function main(cb) {
+    getFiles(function(err, files) {
+        if(err) {
+            return cb(err);
+        }
+
+        parse(files, cb);
+    });
+};
+
+function parse(files, cb) {
+    var base = 'https://raw.githubusercontent.com/jsdelivr/jsdelivr/master/files/';
+    var ret = {};
+
+    async.eachLimit(files, 4, function(file, cb) {
+        var parts = file.split('/');
+        var name = parts[0];
+        var filename, version;
+
+        if(parts.length === 2) {
+            if(parts[1] === 'info.ini') {
+                return parseIni(url.resolve(base, file), function(err, d) {
+                    if(err) {
+                        return setImmediate(cb.bind(null, err));
+                    }
+
+                    if(!(name in ret)) {
+                        ret[name] = {
+                            name: name,
+                            versions: [],
+                            assets: {} // version -> assets
+                        };
+                    }
+
+                    ret[name] = extend(ret[name], d);
+
+                    setImmediate(cb);
+                });
+            }
+
+            return setImmediate(cb);
+        }
+        else {
+            version = parts[1];
+            filename = parts.slice(2).join('/');
+        }
+
+        if(!(name in ret)) {
+            ret[name] = {
+                name: name,
+                versions: [],
+                assets: {} // version -> assets
+            };
+        }
+
+        var lib = ret[name];
+
+        // version
+        if(lib.versions.indexOf(version) === -1) {
+            lib.versions.push(version);
+        }
+
+        // assets
+        if(!(version in lib.assets)) {
+            lib.assets[version] = [];
+        }
+
+        lib.assets[version].push(filename);
+
+        setImmediate(cb);
+    }, function(err) {
+        if(err) {
+            return cb(err);
+        }
+
+        cb(null, values(ret).map(function(v) {
+            v.lastversion = v.versions.slice(-1)[0];
+
+            // convert assets to v1 format
+            var assets = [];
+
+            fp.each(function(version, files) {
+                assets.push({
+                    version: version,
+                    files: files
+                });
+            }, v.assets);
+
+            v.assets = assets;
+
+            return v;
+        }));
+    });
+}
+
+function parseIni(url, cb) {
+    request.get(url, function(err, res, data) {
+        if(err) {
+            return cb(err);
+        }
+
+        cb(null, ini.parse(data));
+    });
+}
+
+function getFiles(cb) {
+    github.repos.getContent({
+        user: 'jsdelivr',
+        repo: 'jsdelivr',
+        path: ''
+    }, function(err, res) {
+        if(err) {
+            return cb(err);
+        }
+
+        var sha = res.filter(function(v) {
+            return v.name === 'files';
+        })[0].sha;
+
+        github.gitdata.getTree({
+            user: 'jsdelivr',
+            repo: 'jsdelivr',
+            sha: sha,
+            recursive: 1
+        }, function(err, res) {
+            if(err) {
+                return cb(err);
+            }
+
+            if(!res.tree) {
+                return cb(new Error('Missing tree'));
+            }
+
+            // mode, 100644 === blob that is file
+            cb(null, res.tree.filter(is('mode', '100644')).map(prop('path')).
+                filter(contains('/')));
+        });
+    });
+}
+
+function is(prop, val) {
+    return function(v) {
+        return v[prop] === val;
+    };
+}
+
+function contains(str) {
+    return function(v) {
+        return v.indexOf(str) >= 0;
+    };
+}
