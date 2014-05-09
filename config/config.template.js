37ea2848250ef47f8890b3605747cb347c13c77e
Juho Vepsalainen <bebraw@gmail.com>
2014-05-09 11:45:02 +0300

    Eliminate redundant configuration

--- config/config.template.js
+++ config/config.template.js
@@ -2,13 +2,6 @@ module.exports = {
     port: 8000,
     output: 'data',
     githubToken: '',
-    mongo: {
-        hostname: 'localhost',
-        port: 27017,
-        db: 'jsdelivr_api_sync',
-        username: '',
-        password: ''
-    },
     tasks: {
         bootstrap: {minute: 0},
         cdnjs: {minute: 2},
