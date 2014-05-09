fe65dd7be0df57b9d979782162a0f901b98b7d2d
Juho Vepsalainen <bebraw@gmail.com>
2014-05-09 09:52:33 +0300

    Serve data through static assets
    Closes #2.

--- serve.js
+++ serve.js
@@ -3,6 +3,11 @@
 
 require('log-timestamp');
 
+var path = require('path');
+
+var async = require('async');
+var connect = require('connect');
+var express = require('express');
 var mkdirp = require('mkdirp');
 var taskist = require('taskist');
 
@@ -31,26 +36,77 @@ if(require.main === module) {
 }
 
 module.exports = main;
-function main(cb) {
-    cb = cb || noop;
+function main() {
+    handleExit();
 
-    mkdirp(config.output, function(err) {
+    async.series([
+        mkdirp.bind(null, config.output),
+        serve.bind(null, config),
+        initTasks
+    ], function(err) {
+        if(err) {
+            return console.error(err);
+        }
+    });
+}
+
+function handleExit() {
+    process.on('exit', terminator);
+
+    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+    'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+    ].forEach(function(element) {
+        process.on(element, function() {
+            terminator(element);
+        });
+    });
+}
+
+function serve(config, cb) {
+    var app = express();
+    var hour = 3600 * 1000;
+    var staticPath = path.join(__dirname, config.output);
+
+    app.use('/data', express['static'](staticPath, {
+        maxAge: hour
+    }));
+
+    var env = process.env.NODE_ENV || 'development';
+    if(env === 'development') {
+        app.use(connect.errorHandler());
+    }
+
+    app.listen(config.port, function(err) {
         if(err) {
             return cb(err);
         }
 
-        console.log('Initializing tasks');
-        initTasks();
+        console.log(
+            'Node (version: %s) %s started on %d ...',
+            process.version,
+            process.argv[1],
+            config.port
+        );
 
         cb();
     });
 }
 
-function initTasks() {
+function initTasks(cb) {
+    console.log('Initializing tasks');
     taskist(config.tasks, tasks, {
-        instant: true,
+        instant: cb,
         series: true
     });
 }
 
-function noop() {}
+function terminator(sig) {
+    if(typeof sig === 'string') {
+        console.log('%s: Received %s - terminating Node server ...',
+            Date(Date.now()), sig);
+
+        process.exit(1);
+    }
+
+    console.log('%s: Node server stopped.', Date(Date.now()) );
+}
